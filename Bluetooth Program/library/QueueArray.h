ce6bb9a6f911fb7c1e0c0f431aee83868ef313cf
TylerFerry24 <33035771+TylerFerry24@users.noreply.github.com>
2017-10-27 13:48:58 -0400

    DFRobot GoBle

--- Bluetooth Program/library/QueueArray.h
+++ Bluetooth Program/library/QueueArray.h
@@ -10,7 +10,7 @@
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *  (at your option) any later version
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -60,8 +60,7 @@
 template<typename T>
 class QueueArray {
   public:
-    // init the queue (constructor).
-    QueueArray ();
+    // init the queue (constructor).    QueueArray ();
 
     // clear the queue (destructor).
     ~QueueArray ();
